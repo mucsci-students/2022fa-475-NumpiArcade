c1c06a73e9cfa40115fa1341874e7aed2b7a560c
CodeDog3 <96511553+CodeDog3@users.noreply.github.com>
2022-10-29 22:06:38 -0400

    weird bug fix

---
+++ Assets/Scripts/Man-Pac/CoinCollect.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinCollect : MonoBehaviour
+{
+    private int collected = 0; 
+    public GameObject collidedWith;
+    public int winCondition;
+
+        void OnTriggerEnter2D(Collider2D coin) 
+        { 
+        if(collidedWith.tag == coin.tag){
+            Debug.Log("collision");
+                collected += 1;
+                Destroy(gameObject);
+        }
+    }
+}
